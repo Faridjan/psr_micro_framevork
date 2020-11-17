a7caf77e6b57692f68a045726e83abf191952353
Farid <fred.andersen7@gmail.com>
2020-11-17 11:52:59 +0600

    Added Simple Pipelene with basic array

---
+++ src/Framework/Http/Pipeline/Pipeline.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Farid\Framework\Http\Pipeline;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Pipeline
+{
+    private $middleware = [];
+
+    public function pipe(callable $middleware): void
+    {
+        $this->middleware[] = $middleware;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $default): ResponseInterface
+    {
+        return $this->next($request, $default);
+    }
+
+    public function next(ServerRequestInterface $request, $default): ResponseInterface
+    {
+        if (!$current = array_shift($this->middleware)) {
+            $default($request);
+        }
+
+        return $current($request, function (ServerRequestInterface $request) use ($default) {
+            return $this->next($request, $default);
+        });
+    }
+}
